29331bcaeaaa46c6723ec31b17e8aa66dfb5496b
Kang Myoung Gon <jin04053@naver.com>
2025-01-08 13:01:30 +0900

    chore: MoveInfo type import 수정

--- src/move/interfaces/move.controller.interface.ts
+++ src/move/interfaces/move.controller.interface.ts
@@ -1,5 +1,5 @@
+import { MoveInfo } from '#move/move.types.js';
 import { FindOptions, RequestFilter } from '#types/options.type.js';
-import { MoveInfo } from '@prisma/client';
 
 export interface IMoveController {
   getMoveInfos: (driverId?: string, options?: FindOptions & RequestFilter) => Promise<{ totalCount: number; list: MoveInfo[] }>;
