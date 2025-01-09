6a3f8c8ac15d8900b1af0c391e6bbbf8b7505802
csbizz <95736373+csbizz@users.noreply.github.com>
2025-01-09 10:08:42 +0900

    feat: findDriver

--- src/drivers/driver.service.ts
+++ src/drivers/driver.service.ts
@@ -1,7 +1,18 @@
+import { DriverNotFoundException } from '#drivers/driver.exception.js';
+import { DriverRepository } from '#drivers/driver.repository.js';
 import { IDriverService } from '#drivers/interfaces/driver.service.interface.js';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class DriverService implements IDriverService {
-  constructor() {}
+  constructor(private readonly driverRepository: DriverRepository) {}
+
+  async findDriver(id: string) {
+    const driver = await this.driverRepository.findById(id);
+    if (!driver) {
+      throw new DriverNotFoundException();
+    }
+
+    return driver;
+  }
 }
--- src/drivers/interfaces/driver.service.interface.ts
+++ src/drivers/interfaces/driver.service.interface.ts
@@ -1,1 +1,5 @@
-export interface IDriverService {}
+import { Driver } from '#drivers/driver.types.js';
+
+export interface IDriverService {
+  findDriver: (id: string) => Promise<Driver>;
+}
