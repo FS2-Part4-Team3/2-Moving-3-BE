ba36d55c8377ac4502ae12168b99fd0065b573d4
csbizz <95736373+csbizz@users.noreply.github.com>
2025-01-09 14:00:00 +0900

    feat: likeDriver, unlikeDriver

--- src/drivers/driver.service.ts
+++ src/drivers/driver.service.ts
@@ -1,18 +1,45 @@
 import { DriverNotFoundException } from '#drivers/driver.exception.js';
 import { DriverRepository } from '#drivers/driver.repository.js';
 import { IDriverService } from '#drivers/interfaces/driver.service.interface.js';
+import { IStorage } from '#types/common.types.js';
 import filterSensitiveData from '#utils/filterSensitiveData.js';
 import { Injectable } from '@nestjs/common';
+import { AsyncLocalStorage } from 'async_hooks';
 
 @Injectable()
 export class DriverService implements IDriverService {
-  constructor(private readonly driverRepository: DriverRepository) {}
+  constructor(
+    private readonly driverRepository: DriverRepository,
+    private readonly als: AsyncLocalStorage<IStorage>,
+  ) {}
 
   async findDriver(id: string) {
     const driver = await this.driverRepository.findById(id);
     if (!driver) {
       throw new DriverNotFoundException();
     }
+
+    return filterSensitiveData(driver);
+  }
+
+  async likeDriver(driverId: string) {
+    const target = await this.driverRepository.findById(driverId);
+    if (!target) throw new DriverNotFoundException();
+
+    const { userId } = this.als.getStore();
+
+    const driver = await this.driverRepository.like(driverId, userId);
+
+    return filterSensitiveData(driver);
+  }
+
+  async unlikeDriver(driverId: string) {
+    const target = await this.driverRepository.findById(driverId);
+    if (!target) throw new DriverNotFoundException();
+
+    const { userId } = this.als.getStore();
+
+    const driver = await this.driverRepository.unlike(driverId, userId);
 
     return filterSensitiveData(driver);
   }
--- src/drivers/interfaces/driver.service.interface.ts
+++ src/drivers/interfaces/driver.service.interface.ts
@@ -3,4 +3,6 @@ import { FilteredPersonalInfo } from '#types/personal.type.js';
 
 export interface IDriverService {
   findDriver: (id: string) => Promise<FilteredPersonalInfo<Driver>>;
+  likeDriver: (driverId: string) => Promise<FilteredPersonalInfo<Driver>>;
+  unlikeDriver: (driverId: string) => Promise<FilteredPersonalInfo<Driver>>;
 }
