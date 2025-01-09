db40a303081607921fb79fc988b3f124114bf3b5
csbizz <95736373+csbizz@users.noreply.github.com>
2025-01-09 09:43:22 +0900

    feat: findDrivers

--- src/drivers/driver.service.ts
+++ src/drivers/driver.service.ts
@@ -1,7 +1,16 @@
+import { DriverRepository } from '#drivers/driver.repository.js';
 import { IDriverService } from '#drivers/interfaces/driver.service.interface.js';
+import { FindOptions } from '#types/options.type.js';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class DriverService implements IDriverService {
-  constructor() {}
+  constructor(private readonly driverRepository: DriverRepository) {}
+
+  async findDrivers(options: FindOptions) {
+    const totalCount = await this.driverRepository.count();
+    const list = await this.driverRepository.findMany(options);
+
+    return { totalCount, list };
+  }
 }
