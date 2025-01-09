28469f00e2238326ce0bfa88b65bd12dde5ca072
csbizz <95736373+csbizz@users.noreply.github.com>
2025-01-09 09:47:43 +0900

    feat: getDrivers

--- src/drivers/driver.controller.ts
+++ src/drivers/driver.controller.ts
@@ -1,14 +1,23 @@
+import { DriverService } from '#drivers/driver.service.js';
 import { IDriverController } from '#drivers/interfaces/driver.controller.interface.js';
-import { Controller, Get } from '@nestjs/common';
+import { GetQueries } from '#types/queries.type.js';
+import { Controller, Get, Query } from '@nestjs/common';
 import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('drivers')
 export class DriverController implements IDriverController {
-  constructor() {}
+  constructor(private readonly driverService: DriverService) {}
 
   @Get()
   @ApiOperation({ summary: '기사 목록 조회' })
-  async getDrivers() {}
+  async getDrivers(@Query() query: GetQueries) {
+    const { page = 1, pageSize = 10, orderBy = 'latest', keyword = '' } = query;
+    const options = { page, pageSize, orderBy, keyword };
+
+    const drivers = await this.driverService.findDrivers(options);
+
+    return drivers;
+  }
 
   @Get(':id')
   @ApiOperation({ summary: '기사 상세 조회' })
