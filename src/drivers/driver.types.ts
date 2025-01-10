b65570f2dbe84f11874a3653d2bbea0de08d137e
csbizz <95736373+csbizz@users.noreply.github.com>
2025-01-10 15:52:48 +0900

    feat: SignUpDTO 분리, DriverEntity 정의

--- src/drivers/driver.types.ts
+++ src/drivers/driver.types.ts
@@ -1,9 +1,30 @@
 import { ModelBase } from '#types/common.types.js';
-import { Driver as PrismaDriver } from '@prisma/client';
+import { PersonalInfo } from '#types/personal.type.js';
+import { $Enums, Driver as PrismaDriver } from '@prisma/client';
+import { ArrayNotEmpty, IsIn, IsString } from 'class-validator';
 
 interface PrismaDriverBase extends Omit<PrismaDriver, keyof ModelBase> {}
 interface DriverBase extends PrismaDriverBase {}
 
 export interface Driver extends DriverBase, ModelBase {}
+
+export class DriverEntity extends PersonalInfo implements Omit<Driver, 'applyCount' | 'likeCount'> {
+  @IsString()
+  nickname: string;
+
+  @IsString()
+  introduce: string;
+
+  @IsString()
+  description: string;
+
+  @ArrayNotEmpty({ message: '하나 이상의 지역을 선택해주세요' })
+  @IsIn(Object.values($Enums.Area), { each: true })
+  availableAreas: $Enums.Area[];
+
+  @ArrayNotEmpty({ message: '하나 이상의 타입을 선택해주세요' })
+  @IsIn(Object.values($Enums.ServiceType), { each: true })
+  serviceTypes: $Enums.ServiceType[];
+}
 
 export interface DriverInputDTO extends Omit<Driver, keyof ModelBase> {}
