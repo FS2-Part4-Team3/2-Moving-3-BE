5cc67c87cdc1e63b898f0b4533f15c1ee51438ec
csbizz <95736373+csbizz@users.noreply.github.com>
2025-01-13 12:42:02 +0900

    fix: 불필요한 유저 메소드 삭제

--- src/users/interfaces/user.controller.interface.ts
+++ src/users/interfaces/user.controller.interface.ts
@@ -1,7 +1,5 @@
 import { User, UserPatchDTO } from '#users/user.types.js';
 
 export interface IUserController {
-  watch: () => void;
-  share: () => void;
   patchUser: (id: string, body: UserPatchDTO) => Promise<User>;
 }
--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,20 +1,12 @@
 import { IUserController } from '#users/interfaces/user.controller.interface.js';
 import { UserService } from '#users/user.service.js';
 import { UserPatchDTO } from '#users/user.types.js';
-import { Body, Controller, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Param, Patch } from '@nestjs/common';
 import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('users')
 export class UserController implements IUserController {
   constructor(private readonly userService: UserService) {}
-
-  @Post('watch')
-  @ApiOperation({ summary: '기사 찜하기' })
-  async watch() {}
-
-  @Post('share')
-  @ApiOperation({ summary: '기사 정보 공유하기' })
-  async share() {}
 
   @Patch(':id/update')
   @ApiOperation({ summary: '유저 정보 수정' })
