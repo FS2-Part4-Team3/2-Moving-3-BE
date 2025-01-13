77c7ee65fcf456b5a18a3eef1670fc6637f9186e
csbizz <95736373+csbizz@users.noreply.github.com>
2025-01-13 12:44:42 +0900

    fix: 유저 정보 수정에 검증단 추가

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,7 +1,8 @@
+import { AccessTokenGuard } from '#guards/access-token.guard.js';
 import { IUserController } from '#users/interfaces/user.controller.interface.js';
 import { UserService } from '#users/user.service.js';
 import { UserPatchDTO } from '#users/user.types.js';
-import { Body, Controller, Param, Patch } from '@nestjs/common';
+import { Body, Controller, Param, Patch, UseGuards } from '@nestjs/common';
 import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('users')
@@ -9,6 +10,7 @@ export class UserController implements IUserController {
   constructor(private readonly userService: UserService) {}
 
   @Patch(':id/update')
+  @UseGuards(AccessTokenGuard)
   @ApiOperation({ summary: '유저 정보 수정' })
   async patchUser(@Param('id') id: string, @Body() body: UserPatchDTO) {
     const user = await this.userService.updateUser(id, body);
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -1,13 +1,19 @@
+import { ForbiddenException } from '#exceptions/http.exception.js';
+import { IStorage } from '#types/common.types.js';
 import { FindOptions } from '#types/options.type.js';
 import { IUserService } from '#users/interfaces/user.service.interface.js';
 import { UserNotFoundException } from '#users/user.exception.js';
 import { UserRepository } from '#users/user.repository.js';
 import { UserPatchDTO } from '#users/user.types.js';
 import { Injectable } from '@nestjs/common';
+import { AsyncLocalStorage } from 'async_hooks';
 
 @Injectable()
 export class UserService implements IUserService {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly als: AsyncLocalStorage<IStorage>,
+  ) {}
 
   async getUsers(options: FindOptions) {
     const totalCount = await this.userRepository.count();
@@ -17,8 +23,15 @@ export class UserService implements IUserService {
   }
 
   async updateUser(id: string, body: UserPatchDTO) {
+    const { userId } = this.als.getStore();
+    if (id !== userId) {
+      throw new ForbiddenException();
+    }
+
     const target = await this.userRepository.findById(id);
-    if (!target) throw new UserNotFoundException();
+    if (!target) {
+      throw new UserNotFoundException();
+    }
 
     const data = body;
     const user = await this.userRepository.update(id, data);
