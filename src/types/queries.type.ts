fc104c9ff608aa6ef481acac2bf1f638ed5dbe14
csbizz <95736373+csbizz@users.noreply.github.com>
2025-01-14 15:02:32 +0900

    docs: patchUser

--- src/types/queries.type.ts
+++ src/types/queries.type.ts
@@ -1,3 +1,4 @@
 import { FindOptions } from '#types/options.type.js';
+import { PartialType } from '@nestjs/swagger';
 
-export interface GetQueries extends Partial<FindOptions> {}
+export class GetQueries extends PartialType(FindOptions) {}
