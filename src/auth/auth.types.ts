81026fc2f67bcb9e43adc8f11016631b4b92bf8a
csbizz <95736373+csbizz@users.noreply.github.com>
2025-01-15 21:41:13 +0900

    refactor: phoneNumberRegex

--- src/auth/auth.types.ts
+++ src/auth/auth.types.ts
@@ -23,7 +23,7 @@ export class SignInDTO {
   password: string;
 }
 
-const numberRegex =
+const phoneNumberRegex =
   /^(010\d{4}\d{4}|02\d{4}\d{4}|032\d{4}\d{4}|042\d{4}\d{4}|051\d{4}\d{4}|052\d{4}\d{4}|053\d{4}\d{4}|062\d{4}\d{4}|064\d{4}\d{4}|031\d{4}\d{4}|033\d{4}\d{4}|041\d{4}\d{4}|043\d{4}\d{4}|054\d{4}\d{4}|055\d{4}\d{4}|061\d{4}\d{4}|063\d{4}\d{4})$/;
 
 export class SignUpDTO {
@@ -45,7 +45,7 @@ export class SignUpDTO {
   salt: string;
 
   @ApiProperty({ description: '전화번호' })
-  @Matches(numberRegex, { message: '올바른 휴대전화 번호를 입력해주세요.' })
+  @Matches(phoneNumberRegex, { message: '올바른 휴대전화 번호를 입력해주세요.' })
   phoneNumber: string;
 }
 
