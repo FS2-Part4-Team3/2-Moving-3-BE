bb145650df76207fbc53ba12f9d6e991a4e387cd
Kang Myoung Gon <jin04053@naver.com>
2025-01-15 23:36:09 +0900

    Fix: getMoveInfos, getMoveInfo

--- src/move/move.controller.ts
+++ src/move/move.controller.ts
@@ -1,10 +1,11 @@
 import { AccessTokenGuard } from '#guards/access-token.guard.js';
 import { IMoveController } from '#move/interfaces/move.controller.interface.js';
 import { FindOptions, RequestFilter } from '#types/options.type.js';
-import { Controller, Get, Patch, Post, Query, UseGuards } from '@nestjs/common';
-import { ApiOperation } from '@nestjs/swagger';
+import { Controller, Get, HttpStatus, Patch, Post, Query, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
 import { MoveService } from './move.service.js';
 import { GetQueries } from '#types/queries.type.js';
+import { BaseMoveInfoOutputDTO, MoveInfoResponseDTO } from './move.types.js';
 
 @Controller('moves')
 export class MoveController implements IMoveController {
@@ -12,7 +13,12 @@ export class MoveController implements IMoveController {
 
   @Get()
   @UseGuards(AccessTokenGuard)
+  @ApiBearerAuth('accessToken')
   @ApiOperation({ summary: '이사 정보 목록 조회' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: MoveInfoResponseDTO,
+  })
   async getMoveInfos(@Query() options: GetQueries & Partial<RequestFilter>) {
     const request = await this.moveService.getMoveInfos(options);
 
@@ -21,7 +27,12 @@ export class MoveController implements IMoveController {
 
   @Get('detail')
   @UseGuards(AccessTokenGuard)
+  @ApiBearerAuth('accessToken')
   @ApiOperation({ summary: '이사 정보 상세 조회' })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: [BaseMoveInfoOutputDTO],
+  })
   async getMoveInfo() {
     const moveInfo = await this.moveService.getMoveInfo();
     return moveInfo;
--- src/move/move.types.ts
+++ src/move/move.types.ts
@@ -1,5 +1,6 @@
 import { ModelBase } from '#types/common.types.js';
-import { MoveInfo as PrismaMoveInfo } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
+import { MoveInfo as PrismaMoveInfo, Progress, ServiceType } from '@prisma/client';
 
 interface PrismaMoveInfoBase extends Omit<PrismaMoveInfo, keyof ModelBase> {}
 interface MoveInfoBase extends PrismaMoveInfoBase {}
@@ -7,3 +8,53 @@ interface MoveInfoBase extends PrismaMoveInfoBase {}
 export interface MoveInfo extends MoveInfoBase, ModelBase {}
 
 export interface MoveInfoInputDTO extends Omit<MoveInfo, keyof ModelBase> {}
+
+export class BaseMoveInfoOutputDTO {
+  @ApiProperty({ description: '이사정보 ID', type: String })
+  id: string;
+
+  @ApiProperty({ description: '작성 날짜', type: String, format: 'date-time' })
+  createdAt: string;
+
+  @ApiProperty({ description: '수정 날짜', type: String, format: 'date-time' })
+  updatedAt: string;
+
+  @ApiProperty({ description: '이사 유형', type: String })
+  serviceType: ServiceType;
+
+  @ApiProperty({ description: '이사 날짜', type: String, format: 'date-time' })
+  date: string;
+
+  @ApiProperty({ description: '출발지', type: String })
+  fromAddress: string;
+
+  @ApiProperty({ description: '도착지', type: String })
+  toAddress: string;
+
+  @ApiProperty({ description: '이사정보 상태', type: String })
+  progress: Progress;
+
+  @ApiProperty({ description: '확정 견적 ID', type: String })
+  confirmedEstimationId: string;
+
+  @ApiProperty({ description: '작성자 ID', type: String })
+  ownerId: string;
+}
+
+class OwnerNameDTO {
+  @ApiProperty({ description: '작성자 이름', type: String })
+  name: String;
+}
+
+class MoveInfoOutputDTO extends BaseMoveInfoOutputDTO {
+  @ApiProperty({ description: '이사 정보 목록', type: OwnerNameDTO })
+  owner: OwnerNameDTO;
+}
+
+export class MoveInfoResponseDTO {
+  @ApiProperty({ description: '전체 이사정보 개수', type: Number })
+  totalCount: number;
+
+  @ApiProperty({ description: '리뷰 목록', type: [MoveInfoOutputDTO] })
+  list: MoveInfoOutputDTO[];
+}
