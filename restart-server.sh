7d04f6202f32e01a8b7a8f6217c176b0c80e6437
csbizz <95736373+csbizz@users.noreply.github.com>
2025-01-16 15:40:41 +0900

    fix: restart

--- restart-server.sh
+++ restart-server.sh
@@ -9,18 +9,18 @@ clear
 echo "Stopping running containers..."
 docker compose down
 
+echo "Removing unused containers..."
+docker container prune -f
+
+echo "Removing unused images..."
+docker image prune -a -f
+
 echo "Checking for processes using port 80..."
 if lsof -i :80 > /dev/null; then
     echo "Killing process on port 80..."
     sudo kill -9 $(lsof -t -i:80)
     sleep 2
 fi
-
-echo "Removing unused containers..."
-docker container prune -f
-
-echo "Removing unused images..."
-docker image prune -a -f
 
 echo "Starting containers..."
 docker compose up --build -d
