1e6132fe6176441ca8201585f511468b608504f2
csbizz <95736373+csbizz@users.noreply.github.com>
2025-01-16 15:00:46 +0900

    fix: entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,17 @@
+# npx prisma generate
+# npx prisma migrate deploy
+# node dist/main
+
 npx prisma generate
-npx prisma migrate deploy
+
+if ! npx prisma migrate deploy; then
+    echo "Migration failed, attempting reset..."
+    npx prisma migrate reset --force
+    
+    if ! npx prisma migrate deploy; then
+        echo "Migration failed even after reset. Exiting..."
+        exit 1
+    fi
+fi
+
 node dist/main
