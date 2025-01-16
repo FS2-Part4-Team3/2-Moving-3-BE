d4d32a525c67f2fcbb4a1cded6a010593e042c7d
csbizz <95736373+csbizz@users.noreply.github.com>
2025-01-16 11:47:15 +0900

    feat: generate s3 download url

--- src/utils/S3/generate-s3-download-url.ts
+++ src/utils/S3/generate-s3-download-url.ts
@@ -3,6 +3,10 @@ import { User } from '#users/user.types.js';
 import { generatePresignedDownloadUrl } from '#utils/S3/generate-presigned-download-url.js';
 
 export async function generateS3DownloadUrl(person: User | Driver) {
+  if (!person.image) {
+    return;
+  }
+
   const uniqueFileName = person.image;
   const s3Key = `image/${person.id}/${uniqueFileName}`;
 
