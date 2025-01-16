77b98543afeceb9f324e62eaa0f72d3a3d980481
csbizz <95736373+csbizz@users.noreply.github.com>
2025-01-16 14:21:47 +0900

    fix: filterSensitiveData type

--- src/utils/filterSensitiveData.ts
+++ src/utils/filterSensitiveData.ts
@@ -1,6 +1,6 @@
 import { Driver } from '#drivers/driver.types.js';
 import { FilteredPersonalInfo } from '#types/personal.type.js';
-import { User } from '@prisma/client';
+import { User } from '#users/user.types.js';
 
 export default function filterSensitiveData<T extends User | Driver>(data: T): FilteredPersonalInfo<T> {
   const { password, salt, refreshToken, ...rest } = data;
