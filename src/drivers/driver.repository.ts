bb9a5f60641dd8323aa63699fcddf76327696dc9
csbizz <95736373+csbizz@users.noreply.github.com>
2025-01-17 16:54:32 +0900

    fix: likedUser 조건 수정

--- src/drivers/driver.repository.ts
+++ src/drivers/driver.repository.ts
@@ -14,6 +14,7 @@ export class DriverRepository implements IDriverRepository {
 
   private generateFindCondition(options: DriversFindOptions) {
     const { orderBy, keyword, area, serviceType, likedUserId } = options;
+    console.log('🚀 ~ DriverRepository ~ generateFindCondition ~ likedUserId:', likedUserId);
 
     let sort = {};
     switch (orderBy) {
@@ -33,12 +34,13 @@ export class DriverRepository implements IDriverRepository {
         sort = { createdAt: 'desc' };
     }
 
+    const likedCondition = likedUserId ? { likedUsers: { some: { id: likedUserId } } } : {};
     const areaCondition = area ? { availableAreas: { has: area } } : {};
     const typeCondition = serviceType ? { serviceType: { has: serviceType } } : {};
 
     const where = {
       OR: [{ name: { contains: keyword } }, { introduce: { contains: keyword } }],
-      likedUsers: { id: likedUserId },
+      ...likedCondition,
       ...areaCondition,
       ...typeCondition,
     };
