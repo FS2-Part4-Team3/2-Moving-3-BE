e5b2c8173ee984c3a5b7bb321ec0bfb47dfdc1a4
csbizz <95736373+csbizz@users.noreply.github.com>
2025-01-18 15:44:17 +0900

    fix: content type

--- src/utils/S3/generate-s3-upload-url.ts
+++ src/utils/S3/generate-s3-upload-url.ts
@@ -3,11 +3,24 @@ import { generatePresignedUploadUrl } from '#utils/S3/generate-presigned-upload-
 export async function generateS3UploadUrl(id: string, originalFileName: string) {
   const uniqueFileName = `${Date.now()}.${originalFileName}`;
   const s3Key = `image/${id}/${uniqueFileName}`;
-  // const extension = originalFileName.split('.').at(-1);
-  // const type = extension === 'jpg' ? 'jpeg' : extension;
-  // const contentType = `image/${type}`;
+  const extension = originalFileName.split('.').at(-1);
+  let type;
+  switch (extension) {
+    case 'jpg':
+      type = 'jpeg';
+      break;
+    case 'svg':
+      type = 'svg+xml';
+      break;
+    case 'ico':
+      type = 'vnd.microsoft.icon';
+      break;
+    default:
+      type = extension;
+  }
+  const contentType = `image/${type}`;
 
-  const uploadUrl = await generatePresignedUploadUrl(s3Key);
+  const uploadUrl = await generatePresignedUploadUrl(s3Key, contentType);
 
   return { uploadUrl, uniqueFileName };
 }
