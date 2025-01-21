bf1f9d6928bc3f97265b7232739cf1072ff2eab2
csbizz <95736373+csbizz@users.noreply.github.com>
2025-01-21 10:46:40 +0900

    fix: dist to dist/src

--- src/notifications/notification.gateway.ts
+++ src/notifications/notification.gateway.ts
@@ -1,4 +1,5 @@
 import { INotificationGateway } from '#notifications/interfaces/notification.gateway.interface.js';
+import { WebSocketGateway } from '@nestjs/websockets';
 
-// @WebSocketGateway();
+@WebSocketGateway()
 export class NotificationGateway implements INotificationGateway {}
