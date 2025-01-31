b698c99814ed3bfc9d6a4579710e7458af956a2d
csbizz <95736373+csbizz@users.noreply.github.com>
2025-01-31 16:09:38 +0900

    feat: 알림 메세지 추가 및 D-1 추가

--- src/utils/dateUtils.ts
+++ src/utils/dateUtils.ts
@@ -6,6 +6,10 @@ export function getDayEnd(date: Date) {
   return new Date(getDayStart(date).setHours(23, 59, 59, 999));
 }
 
+export function getNextDate(date: Date) {
+  return new Date(date.getTime() + 1 * 24 * 60 * 60 * 1000);
+}
+
 export function getNextWeek(date: Date) {
   return new Date(date.getTime() + 7 * 24 * 60 * 60 * 1000);
 }
