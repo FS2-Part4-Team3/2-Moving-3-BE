1e2b43b98115e150b1e19ee6f3684cab149e1f59
csbizz <95736373+csbizz@users.noreply.github.com>
2025-01-31 12:28:28 +0900

    refactor: question dto from omittype to picktype

--- src/questions/types/question.dto.ts
+++ src/questions/types/question.dto.ts
@@ -1,13 +1,13 @@
 import { Question, QuestionEntity } from '#questions/types/question.types.js';
 import { ModelBase } from '#types/common.types.js';
-import { OmitType, PartialType } from '@nestjs/swagger';
+import { PartialType, PickType } from '@nestjs/swagger';
 
 interface PersonIds {
   userId: string;
   driverId: string;
 }
 
-export class QuestionPostDTO extends OmitType(QuestionEntity, ['estimationId']) {}
+export class QuestionPostDTO extends PickType(QuestionEntity, ['content']) {}
 export interface QuestionCreateDTO extends Omit<Question, keyof (ModelBase & PersonIds)> {
   userId?: string;
   driverId?: string;
