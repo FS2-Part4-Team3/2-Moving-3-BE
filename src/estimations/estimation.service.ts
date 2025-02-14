3fb0e4a032022051d6a74f293f82dd5b802a72ab
yoonseo <meowyooon@gmail.com>
2025-02-14 16:47:02 +0900

    feat:임시커밋

--- src/estimations/estimation.service.ts
+++ src/estimations/estimation.service.ts
@@ -123,9 +123,14 @@ export class EstimationService implements IEstimationService {
   async getUserEstimationList(options: EstimationGetQueries): Promise<UserEstimationListWithCountDTO> {
     const { userId } = this.als.getStore();
     if (!userId) throw new UnauthorizedException();
+    console.log('ususususu', userId);
 
     const { page, pageSize } = options;
-    const { estimations, totalCount } = await this.estimationRepository.findUserEstimations(userId, page, pageSize);
+    const totalCount = await this.estimationRepository.getTotalCountForUser(userId);
+    console.log('~~~~~~~~~~~~!!!!!!!!!!Total Count:', totalCount);
+
+    const estimations = await this.estimationRepository.findUserEstimations(userId, page, pageSize);
+    console.log('~~~~~~~~~~~~~~~~~~~~~~~!!!!Estimations:', estimations);
 
     const estimationsWithInfo = await Promise.all(
       estimations.map(async estimation => {
@@ -133,7 +138,9 @@ export class EstimationService implements IEstimationService {
         const isLiked = await this.driversService.isLikedDriver(estimation.driverId);
         const moveInfo = await this.moveRepository.findByMoveInfoId(estimation.moveInfoId);
 
-        const isSpecific = await this.estimationRepository.isSpecificEstimation(estimation.moveInfoId);
+        const designatedRequest = await this.requestRepository.DesignatedRequest(estimation.moveInfoId, estimation.driverId);
+        console.log('Designated~~~~~~~~~~~~~~~~~~~', estimation.id, designatedRequest);
+        console.log(`견적 ${estimation.id} - 지정 견적 여부: ${designatedRequest}`);
 
         return {
           driver: {
@@ -157,7 +164,7 @@ export class EstimationService implements IEstimationService {
             estimationId: estimation.id,
             price: estimation.price ?? null,
           },
-          designatedRequest: isSpecific ? IsActivate.Active : IsActivate.Inactive, // 지정/일반 여부
+          designatedRequest, // //'Active': 지정 요청 견적, 'Inactive': 일반 견적
         };
       }),
     );
