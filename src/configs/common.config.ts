019b82ecd7539c1b0d84ca2d19f005beac351a8c
csbizz <95736373+csbizz@users.noreply.github.com>
2025-02-14 10:35:07 +0900

    hotfix: oauth redirect check

--- src/configs/common.config.ts
+++ src/configs/common.config.ts
@@ -2,10 +2,6 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-if (!process.env.OAUTH_REDIRECT) {
-  throw new Error('OAUTH_REDIRECT 환경변수가 설정되지 않았습니다.');
-}
-
 export const port = process.env.PORT || 3000;
 export const nodeEnv = process.env.NODE_ENV || 'production';
 export const oauthRedirect = process.env.OAUTH_REDIRECT || 'http://localhost:3000';
