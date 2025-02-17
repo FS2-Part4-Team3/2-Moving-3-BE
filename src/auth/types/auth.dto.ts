0df5f45c3ee4250c4c0b00ca3b31b416b62a5f63
csbizz <95736373+csbizz@users.noreply.github.com>
2025-02-17 13:49:30 +0900

    fix: api docs

--- src/auth/types/auth.dto.ts
+++ src/auth/types/auth.dto.ts
@@ -1,4 +1,9 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class LoggedInUsersDTO {
+  @ApiProperty({ description: '대상 ID' })
   id: string;
+
+  @ApiProperty({ description: '온라인 여부' })
   isOnline: boolean;
 }
