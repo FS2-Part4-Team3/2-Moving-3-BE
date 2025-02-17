1c132acaaa8a6f0ba63a4dc3ace69d09e65b77f4
csbizz <95736373+csbizz@users.noreply.github.com>
2025-02-17 14:17:12 +0900

    refactor: notification

--- src/notifications/interfaces/notification.service.interface.ts
+++ src/notifications/interfaces/notification.service.interface.ts
@@ -1,8 +1,8 @@
 import { NotificationCreateDTO } from '#notifications/types/notification.dto.js';
-import { Notification } from '#notifications/types/notification.types.js';
+import { INotification } from '#notifications/types/notification.types.js';
 
 export interface INotificationService {
-  getNotifications: (page: number, pageSize: number) => Promise<{ totalCount: number; list: Notification[] }>;
-  createNotification: (body: NotificationCreateDTO) => Promise<Notification>;
-  markNotificationAsRead: (notificationIds: string[]) => Promise<Notification[]>;
+  getNotifications: (page: number, pageSize: number) => Promise<{ totalCount: number; list: INotification[] }>;
+  createNotification: (body: NotificationCreateDTO) => Promise<INotification>;
+  markNotificationAsRead: (notificationIds: string[]) => Promise<INotification[]>;
 }
--- src/notifications/types/notification.types.ts
+++ src/notifications/types/notification.types.ts
@@ -1,7 +1,27 @@
 import { ModelBase } from '#types/common.types.js';
-import { Notification as PrismaNotification } from '@prisma/client';
 
-interface PrismaNotificationBase extends Omit<PrismaNotification, keyof ModelBase> {}
-interface NotificationBase extends PrismaNotificationBase {}
+export enum NotificationType {
+  MOVE_INFO_EXPIRED = 'MOVE_INFO_EXPIRED',
+  NEW_REQUEST = 'NEW_REQUEST',
+  NEW_ESTIMATION = 'NEW_ESTIMATION',
+  REQUEST_REJECTED = 'REQUEST_REJECTED',
+  ESTIMATION_CONFIRMED = 'ESTIMATION_CONFIRMED',
+  NEW_QUESTION = 'NEW_QUESTION',
+  D_7 = 'D_7',
+  D_1 = 'D_1',
+  D_DAY = 'D_DAY',
+}
 
-export interface Notification extends NotificationBase, ModelBase {}
+export interface INotification extends ModelBase {
+  type: NotificationType;
+  message: string;
+  isRead: boolean;
+
+  userId?: string;
+  driverId?: string;
+
+  moveInfoId?: string;
+  requestId?: string;
+  estimationId?: string;
+  questionId?: string;
+}
