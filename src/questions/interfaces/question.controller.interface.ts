b2fd16907cee56428a58a2de99b64ba5ceb0fd5e
csbizz <95736373+csbizz@users.noreply.github.com>
2025-02-17 14:19:10 +0900

    refactor: question

--- src/questions/interfaces/question.controller.interface.ts
+++ src/questions/interfaces/question.controller.interface.ts
@@ -1,8 +1,8 @@
 import { QuestionPatchDTO } from '#questions/types/question.dto.js';
-import { Question } from '#questions/types/question.types.js';
+import { IQuestion } from '#questions/types/question.types.js';
 
 export interface IQuestionController {
-  getQuestion: (id: string) => Promise<Question>;
-  patchQuestion: (id: string, body: QuestionPatchDTO) => Promise<Question>;
-  deleteQuestion: (id: string) => Promise<Question>;
+  getQuestion: (id: string) => Promise<IQuestion>;
+  patchQuestion: (id: string, body: QuestionPatchDTO) => Promise<IQuestion>;
+  deleteQuestion: (id: string) => Promise<IQuestion>;
 }
